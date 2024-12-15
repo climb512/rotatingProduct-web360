1582867159aef2541508438269236567f1e3601b
Ted Rinquest <climb512@gmail.com>
2024-12-15 19:21:49 +0700

    update path

--- scripts/createRotator.js
+++ scripts/createRotator.js
@@ -2,13 +2,13 @@
 var rotator = {};
 function make360(subject) {
     rotator = WR360.ImageRotator.Create('animation');
-    rotator.licenseFileURL = './wr/license.lic';
+    rotator.licenseFileURL = '/rotatingProduct-web360/wr/license.lic';
     if (subject == 'umbrella') {
-        rotator.settings.configFileURL = './wr/360_assets/heroumbrella1.7/heroumbrella1.7.xml'; // Specific to this page
+        rotator.settings.configFileURL = '/rotatingProduct-web360/wr/360_assets/heroumbrella1.7/heroumbrella1.7.xml'; // Specific to this page
     } else {
-        rotator.settings.configFileURL = './wr/360_assets/testRotatingFan3.12/testRotatingFan3.12.xml'; // Specific to this page
+        rotator.settings.configFileURL = '/rotatingProduct-web360/wr/360_assets/testRotatingFan3.12/testRotatingFan3.12.xml'; // Specific to this page
     }
-    rotator.settings.graphicsPath = './wr/imagerotator/html/img/basic';
+    rotator.settings.graphicsPath = '/rotatingProduct-web360/wr/imagerotator/html/img/basic';
     rotator.settings.googleEventTracking = false;
     rotator.settings.responsiveBaseWidth = 0;
     rotator.settings.responsiveMinHeight = 0;
